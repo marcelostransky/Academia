2c783b6ea3dc168799da2f9ebb3fe03aea37d994
Marcelo Benes Stransky Silva <marcelostransky@gmail.com>
2019-06-16 10:34:05 -0300

    Start contexto mensalidade. UI, comando e contexto

---
+++ Academia.Io.Tests/Entidades/MensalidadeTests.cs
@@ -0,0 +1,35 @@
+﻿using AcademiaDanca.IO.Dominio.Contexto.Entidade;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Academia.Io.Tests.Entidades
+{
+    [TestClass]
+    public class MensalidadeTests
+    {
+        int Dia = 5;
+        Mensalidade mensalidade = new Mensalidade(0, 1, 5, Convert.ToDecimal(568.32), 20, Convert.ToDateTime("30/03/2019"));
+        [TestMethod]
+        public void DeveRetornar12datasVencimentoPorDia()
+        {
+            DateTime DataInicial = Convert.ToDateTime($"{Dia}/03/2019");
+            int TotalParcelas = 12;
+            List<DateTime> Datas = new List<DateTime>();
+            for (int i = 1; i <= TotalParcelas; i++)
+            {
+                Datas.Add(DataInicial);
+                DataInicial = DataInicial.AddMonths(1);
+            }
+            Assert.AreEqual(12, Datas.Count);
+        }
+
+        [TestMethod]
+        public void DeveRetornar5Mensalidades()
+        {
+            var retorno = mensalidade.Mensalidades();
+            Assert.AreEqual(5, retorno.Count);
+        }
+    }
+}
---
+++ AcademiaDanca.Dominio/Contexto/Entidade/Mensalidade.cs
@@ -0,0 +1,47 @@
+﻿using FluentValidator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AcademiaDanca.IO.Dominio.Contexto.Entidade
+{
+    public class Mensalidade : Notifiable
+    {
+        public int IdAluno { get; private set; }
+        public int Id { get; private set; }
+        public int DiaPagamento { get; private set; }
+        public int Parcela { get; private set; }
+        public DateTime DataVencimento { get;private set; }
+        public decimal Valor { get; private set; }
+        public decimal Desconto { get; private set; }
+
+
+        public Mensalidade(
+            int id,
+            int idAluno, 
+            int parcela, 
+            decimal valor, 
+            decimal desconto, 
+            DateTime dataVencimento)
+        {
+            Id = id;
+            IdAluno = idAluno;
+            Parcela = parcela;
+            Valor = valor;
+            Desconto = desconto;
+            DataVencimento = dataVencimento;
+        }
+
+        public IList<Mensalidade> Mensalidades()
+        {
+            var mensalidadesRetorno = new List<Mensalidade>();
+            var data = this.DataVencimento;
+            for (int i = 1; i <= Parcela; i++)
+            {
+                mensalidadesRetorno.Add(new Mensalidade(Id, IdAluno, i, Valor, Desconto, data));
+                data = data.AddMonths(1);
+            }
+            return mensalidadesRetorno;
+        }
+    }
+}
